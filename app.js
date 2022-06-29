b2a003431eff2a9b49b6cb746ff2dbb31d7409e9
Yuleni1 <myuleni4@gmail.com>
2022-06-29 17:52:50 -0400

    complete

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//core module
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site');
 const inquirer = require('inquirer');
 //local module
 const generatePage = require('./src/page-template');
@@ -148,17 +147,58 @@ if (!portfolioData.projects) {
 //   github: 'lernantino',
 //   projects: []
 // }
-  promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
+//   promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     const pageHTML = generatePage(portfolioData);
 // const pageHTML = generatePage(mockData);
 
     //writes the html file
-     fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log('Page created! Check out index.html in this directory to see it!');
+      
+//         fs.copyFile('./src/style.css', './dist/style.css', err => {
+//           if (err) {
+//             console.log(err);
+//             return;
+//           }
+//           console.log('Style sheet copied successfully!');
+//         });
+//       });
+//   });
 
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-  });
+//We start by asking the user for their information with Inquirer prompts; this returns all of the data as an object in a Promise.
+promptUser()
+//The promptProject() function captures the returning data from promptUser() and we recursively call promptProject() for as many projects as the user wants to add. Each project
+//will be pushed into a projects array in the collection of portfolio information, and when we're done, the final set of data is returned to the next .then().
+.then(promptProject)
 
+//The finished portfolio data object is returned as portfolioData and sent into the generatePage() function, 
+//which will return the finished HTML template code into pageHTML.
+.then(portfolioData => {
+  return generatePage(portfolioData);
+})
+//We pass pageHTML into the newly created writeFile() 
+//function, which returns a Promise. This is why we use return here, so the Promise is returned into the next .then() method.
+.then(pageHTML => {
+  return writeFile(pageHTML);
+})
+//Upon a successful file creation, we take the writeFileResponse object
+// provided by the writeFile() function's resolve() execution to log it, and then we return copyFile().
+.then(writeFileResponse => {
+  console.log(writeFileResponse);
+  return copyFile();
+})
+
+//The Promise returned by copyFile() then lets us know if the CSS file was copied correctly, and if so, we're all done!
+.then(copyFileResponse => {
+  console.log(copyFileResponse);
+})
+//We only need to write one .catch() method to handle any error that may occur with any of the Promise-based functions. 
+.catch(err => {
+  console.log(err);
+});
